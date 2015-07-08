c0f20f2956613e70394339d9ba792b5c6d6da9ed
unknown <yyjess@gmail.com>
2015-07-08 22:29:28 +0800

    changed hello.py

--- Chapter02/hello.py
+++ Chapter02/hello.py
@@ -1,3 +1,11 @@
-print "This is CS902."
-print "Hello, everyone!"
-print "Let the journey begin!"
+# This program says "Hello" and asks for your name
+print "Hello! Welcome to CS902!"
+print
+print
+print "What is your name?"
+myName = raw_input()
+print
+print
+print "It is good to meet you, " + myName +"!"
+print
+print
