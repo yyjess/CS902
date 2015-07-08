bed9bd6f52162ead3fc81be6b436dcea28ee5dfe
unknown <yyjess@gmail.com>
2015-07-09 00:01:07 +0800

    favorate

--- Chapter02/favorite2_changeVarName.py
+++ Chapter02/favorite2_changeVarName.py
@@ -1,20 +1,20 @@
 # Favorite stuff
 print 'Tell me what your favorite color is.'
-favoriteColor = raw_input()
+meaningless = raw_input()
 print
 print 'Tell me what your favorite animal is.'
-favoriteAnimal = raw_input()
+fj_1 = raw_input()
 print
 print 'Tell me what your favorite food is.'
-favoriteFood = raw_input()
+whatisthis = raw_input()
 print
 print
 
 #display the stuff
 print 'Here is the list of your favorite stuff:'
-print 'Color: ' + favoriteColor
-print 'Animal: ' + favoriteAnimal
-print 'Food: ' + favoriteFood
+print 'Color: ' + meaningless
+print 'Animal: ' + fj_1
+print 'Food: ' + whatisthis
 print
 print
 print "That's what i know about your favorite things."
